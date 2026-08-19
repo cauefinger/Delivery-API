91e6a69919f8c38e898c8d5ccef03c4273c00057
Caue Finger <fingercaue@gmail.com>
2026-08-19 11:28:43 -0300

    rota de cancelar pedido adicionada

--- order_routes.py
+++ order_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from dependencies import pegar_sessao, verificar_token
 from schemas import PedidoSchema 
@@ -18,3 +18,16 @@ async def criar_pedido(pedido_schema: PedidoSchema, sessao: Session = Depends(pe
     sessao.add(novo_pedido)
     sessao.commit()
     return {"mensagem": f"Pedido criado com sucesso. ID do pedido: {novo_pedido.id}"}
+
+
+@order_router.post("/pedido/cancelar`{pedido_id}")
+async def cancelar_pedido(pedido_id: int,  sessao: Session = Depends(pegar_sessao)):  
+    pedido = sessao.query(Pedido).filter(pedido_id == Pedido.id).first()
+    if not pedido:
+        raise HTTPException(status_code=400, detail="Pedido não encontrado.")
+    pedido.status = "CANCELADO"
+    sessao.commit()
+    return {
+        "mensagem":f"Pedido de número: {pedido_id} cancelado com sucesso.",
+        "pedido": pedido
+    }
